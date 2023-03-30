638b1b96ff639e4363afd16f4b7dc96eddd48b4d
ohashin2G <98792149+ohashin2G@users.noreply.github.com>
2023-03-30 03:27:22 +0000

    Deleted egg-info file

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup(name='Demo',
-      version='0.1',
-      description='A Book Lover package',
-      url='https://github.com/ohashin2G/DS5100_Project.git',
-      author='N. Ohashi',
-      author_email='fju4ek@virginia.edu',
-      license='MIT',
-      packages=['Demo'])
+setup(
+    name='Demo',
+    version='0.1',
+    description='A Book Lover package',
+    url='https://github.com/ohashin2G/HW09_BookLover',
+    author='N. Ohashi',
+    author_email='fju4ek@virginia.edu',
+    license='MIT',
+    packages=['Demo'],
+)
