92babf38136fa85dbd2d9cd41704f9db8eeca1d5
Naomi Ohashi <ohashin2G@gmail.com>
2023-03-30 00:23:59 -0400

    Added practice file

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='Demo',
+    name='Demo.booklover',
     version='0.1',
     description='A Book Lover package',
-    url='https://github.com/ohashin2G/HW09_BookLover',
     author='N. Ohashi',
     author_email='fju4ek@virginia.edu',
     license='MIT',
-    packages=['Demo'],
+    packages=['Demo.booklover'],
 )
