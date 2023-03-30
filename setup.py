e1b9f4f5779bb43bdfc85195fc3bc9d3a7b0f903
ohashin2G <98792149+ohashin2G@users.noreply.github.com>
2023-03-29 22:16:41 -0400

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A Book Lover package',
+      url='https://github.com/ohashin2G/DS5100_Project.git',
+      author='N. Ohashi',
+      author_email='fju4ek@virginia.edu',
+      license='MIT',
+      packages=['Demo'])
